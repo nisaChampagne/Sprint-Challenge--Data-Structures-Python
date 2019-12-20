8d69a0dee9b2cd4d3dbd345fdab290c17371204b
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-12-20 12:04:35 -0500

    using BST in names.py got runtime down to 0.0 seconds?? cool

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BinarySearchTree
 
 start_time = time.time()
 
@@ -19,6 +20,30 @@ for name_1 in names_1:
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`")
+
+# #OPTION 1:  cant use set though blah
+# duplicates = set(names_1) & set(names_2)
+# end_time = time.time()
+
+# OPTION 2:
+start_time2 = time.time()
+end_time2 = time.time()
+
+bst = BinarySearchTree('names')
+for name2 in names_2:
+    bst.insert(name2)
+
+duplicates2 = []
+for name_1 in names_1:
+    if bst.contains(name_1):
+        duplicates2.append(name_1)
+
+print(f"{len(duplicates2)} duplicates: \n\n{', '.join(duplicates2)}\n\n")
+print(f"runtime: {end_time2 - start_time2} seconds")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`")
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
